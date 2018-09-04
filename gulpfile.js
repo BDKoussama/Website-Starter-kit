6bcd39d9a7fbac9ad7c7416c79de0e70f0a138b5
BDKoussama <33638203+BDKoussama@users.noreply.github.com>
2018-09-05 00:52:11 +0100

    transpile ES6 task

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,19 @@ var autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     del = require('del'),
     gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    webpack = require('webpack-stream'),
     minifycss = require('gulp-minify-css'),
+    minifyJs = require('gulp-minify'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify-es').default,
     images = require('gulp-imagemin'),
     browserSync = require('browser-sync').create();
-
 
 // paths
 var styleSrc = 'source/sass/**/*.sass',
@@ -26,43 +30,43 @@ var styleSrc = 'source/sass/**/*.sass',
     scriptDest = 'build/assets/js/';
 
 
-
 // --------------------------------------------
 // Stand Alone Tasks                           |
 // --------------------------------------------
 
 
 // Compiles all SASS files
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('source/sass/**/*.sass')
         .pipe(plumber())
         .pipe(sass({
             style: 'compressed'
         }))
+        .pipe(postcss([autoprefixer({
+            browsers: 'last 2 versions, > 2%'
+        })]))
         .pipe(rename({
-            basename: 'main',
-            suffix: '.min'
-          }))
-
+            basename: 'main'
+        }))
         .pipe(gulp.dest('build/assets/css'));
 });
 
-gulp.task('images', function() {
+
+gulp.task('images', function () {
     gulp.src('source/img/*')
         .pipe(images())
         .pipe(gulp.dest('build/assets/img'));
 });
 
-// Uglify js files
-gulp.task('scripts', function() {
-    gulp.src('source/js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
+// transpile Es6 (webpack / babel)
+gulp.task('scripts', function () {
+    return gulp.src('source/js/app.js')
+        .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('build/assets/js'));
 });
 
 //Concat and Compress Vendor .js files
-gulp.task('vendors', function() {
+gulp.task('vendors', function () {
     gulp.src(
             [
                 'source/js/vendors/jquery.min.js',
@@ -75,10 +79,8 @@ gulp.task('vendors', function() {
 });
 
 
-
 // Watch for changes
-gulp.task('watch', function(){
-
+gulp.task('watch', function () {
     // Serve files from the root of this project
     browserSync.init({
         server: {
@@ -87,13 +89,13 @@ gulp.task('watch', function(){
         notify: false
     });
 
-    gulp.watch(styleSrc,['sass']);
-    gulp.watch(scriptSrc,['scripts']);
-    gulp.watch(vendorSrc,['vendors']);
+    gulp.watch(styleSrc, ['sass']);
+    gulp.watch(scriptSrc, ['scripts']);
+    gulp.watch(vendorSrc, ['vendors']);
     gulp.watch(['build/*.html', 'build/assets/css/*.css', 'build/assets/js/*.js', 'build/assets/js/vendors/*.js']).on('change', browserSync.reload);
 
 });
 
 
 // use default task to launch Browsersync and watch JS files
-gulp.task('default', [ 'sass', 'scripts', 'vendors', 'watch'], function () {});
+gulp.task('default', ['sass', 'scripts', 'vendors', 'watch'], function () {});
